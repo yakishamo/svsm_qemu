a4b62322d17922d8496f9814901fdd6b1e595667
Andreas Färber <afaerber@suse.de>
2016-08-10 19:00:24 +0200

    [openSUSE] qemu-binfmt-conf: Modify default path
    Change QEMU_PATH from /usr/local/bin to /usr/bin prefix.
    
    Signed-off-by: Andreas Färber <afaerber@suse.de>

--- scripts/qemu-binfmt-conf.sh
+++ scripts/qemu-binfmt-conf.sh
@@ -341,7 +341,7 @@ BINFMT_SET=qemu_register_interpreter
 SYSTEMDDIR="/etc/binfmt.d"
 DEBIANDIR="/usr/share/binfmts"
 
-QEMU_PATH=/usr/local/bin
+QEMU_PATH=/usr/bin
 CREDENTIAL=no
 PERSISTENT=no
 PRESERVE_ARG0=no
