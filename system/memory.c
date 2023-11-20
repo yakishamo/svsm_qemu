69791f0a9520e8c1afced7e9c9fc71a5c130468d
Philippe Mathieu-Daudé <philmd@linaro.org>
2023-11-20 11:29:31 +0100

    memory: Have memory_region_init_rom() handler return a boolean
    Following the example documented since commit e3fe3988d7 ("error:
    Document Error API usage rules"), have memory_region_init_rom()
    return a boolean indicating whether an error is set or not.
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Manos Pitsidianakis <manos.pitsidianakis@linaro.org>
    Reviewed-by: Gavin Shan <gshan@redhat.com>
    Message-Id: <20231120213301.24349-8-philmd@linaro.org>

--- system/memory.c
+++ system/memory.c
@@ -3682,19 +3682,16 @@ void memory_region_init_ram_guest_memfd(MemoryRegion *mr,
     vmstate_register_ram(mr, owner_dev);
 }
 
-void memory_region_init_rom(MemoryRegion *mr,
+bool memory_region_init_rom(MemoryRegion *mr,
                             Object *owner,
                             const char *name,
                             uint64_t size,
                             Error **errp)
 {
     DeviceState *owner_dev;
-    Error *err = NULL;
 
-    memory_region_init_rom_nomigrate(mr, owner, name, size, &err);
-    if (err) {
-        error_propagate(errp, err);
-        return;
+    if (!memory_region_init_rom_nomigrate(mr, owner, name, size, errp)) {
+        return false;
     }
     /* This will assert if owner is neither NULL nor a DeviceState.
      * We only want the owner here for the purposes of defining a
@@ -3704,6 +3701,8 @@ void memory_region_init_rom(MemoryRegion *mr,
      */
     owner_dev = DEVICE(owner);
     vmstate_register_ram(mr, owner_dev);
+
+    return true;
 }
 
 void memory_region_init_rom_device(MemoryRegion *mr,
