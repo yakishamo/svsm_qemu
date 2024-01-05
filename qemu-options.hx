5c9662ed2cd61d4042868897cacc407b87278dd9
Roy Hopkins <roy.hopkins@suse.com>
2024-01-05 10:09:43 +0000

    backends/confidential-guest-support: Add IGVM file parameter
    In order to add support for parsing IGVM files for secure virtual
    machines, a the path to an IGVM file needs to be specified as
    part of the guest configuration. It makes sense to add this to
    the ConfidentialGuestSupport object as this is common to all secure
    virtual machines that potentially could support IGVM based
    configuration.
    
    This patch allows the filename to be configured via the QEMU
    object model in preparation for subsequent patches that will read and
    parse the IGVM file.
    
    Signed-off-by: Roy Hopkins <roy.hopkins@suse.com>

--- qemu-options.hx
+++ qemu-options.hx
@@ -5637,7 +5637,7 @@ SRST
                  -object secret,id=sec0,keyid=secmaster0,format=base64,\\
                      data=$SECRET,iv=$(<iv.b64)
 
-    ``-object sev-guest,id=id,cbitpos=cbitpos,reduced-phys-bits=val,[sev-device=string,policy=policy,handle=handle,dh-cert-file=file,session-file=file,kernel-hashes=on|off]``
+    ``-object sev-guest,id=id,cbitpos=cbitpos,reduced-phys-bits=val,[sev-device=string,policy=policy,handle=handle,dh-cert-file=file,session-file=file,kernel-hashes=on|off,igvm-file=file]``
         Create a Secure Encrypted Virtualization (SEV) guest object,
         which can be used to provide the guest memory encryption support
         on AMD processors.
@@ -5680,6 +5680,12 @@ SRST
         The ``kernel-hashes`` adds the hashes of given kernel/initrd/
         cmdline to a designated guest firmware page for measured Linux
         boot with -kernel. The default is off. (Since 6.2)
+
+        The ``igvm-file`` is an optional parameter that, when specified,
+        allows an Independent Guest Virtual Machine (IGVM) file to be
+        specified that configures the secure virtual machine and can
+        include, for example, an SVSM module, system firmware, initial
+        boot state, etc.
 
         e.g to launch a SEV guest
 
@@ -5844,7 +5850,6 @@ SRST
 
             (qemu) qom-set /objects/iothread1 poll-max-ns 100000
 ERST
-
 
 HXCOMM This is the last statement. Insert new options before this line!
 
