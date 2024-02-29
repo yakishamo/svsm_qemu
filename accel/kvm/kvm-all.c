42b4d4d8d499c6d347e27353694f047ce31d330b
Xiaoyao Li <xiaoyao.li@intel.com>
2024-02-29 01:36:31 -0500

    kvm/memory: Make memory type private by default if it has guest memfd backend
    KVM side leaves the memory to shared by default, while may incur the
    overhead of paging conversion on the first visit of each page. Because
    the expectation is that page is likely to private for the VMs that
    require private memory (has guest memfd).
    
    Explicitly set the memory to private when memory region has valid
    guest memfd backend.
    
    Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>

--- accel/kvm/kvm-all.c
+++ accel/kvm/kvm-all.c
@@ -1451,7 +1451,7 @@ static void kvm_set_phys_mem(KVMMemoryListener *kml,
             abort();
         }
 
-        if (memory_region_is_default_private(mr)) {
+        if (memory_region_has_guest_memfd(mr)) {
             err = kvm_set_memory_attributes_private(start_addr, slot_size);
             if (err) {
                 error_report("%s: failed to set memory attribute private: %s\n",
